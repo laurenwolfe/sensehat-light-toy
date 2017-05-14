ac621c61410ff1a0dc0f78166398a0b2ead4d328
Lauren Wolfe <wolfela@uw.edu>
2017-05-14 11:50:34 -0700

    update

--- src/main.py
+++ src/main.py
@@ -137,8 +137,8 @@ def overwrite_grid(grid, color_list, total_rings):
 
     # output color rings
     if num_color_rings > 0:
-        print("test color -  left: {}, right: {}".format(tmp_left, tmp_right))
         for i in range(0, num_color_rings):
+            print("test color -  left: {}, right: {}".format(tmp_left, tmp_right))
             if tmp_left < 0:
                 tmp_left = left
             if tmp_right >= GRID_SIZE:
