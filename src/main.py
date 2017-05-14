8bf480d536e7991c0ed3e1016492675233996630
Lauren Wolfe <wolfela@uw.edu>
2017-05-14 14:57:13 -0700

    update

--- src/main.py
+++ src/main.py
@@ -147,12 +147,12 @@ def overwrite_grid(grid, sense, color_list, total_rings):
         idx = 0
 
         for el in rings:
+            print("left: {}, right: {}, idx: {}".format(left, right, idx))
             if el is not None:
                 grid[left][int(left + idx)] = el
                 grid[right][int(left + idx)] = el
                 grid[int(left + idx)][left] = el
                 grid[int(left + idx)][right] = el
-                print("left: {}, right: {}, idx: {}, el: {}".format(left, right, idx, el))
             else:
                 print("rings is None at index {}".format(idx))
             idx += 1
